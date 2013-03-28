075fae7baa5a367bc6679c2864b9bee105f0798b
Marc Kuo <kuomarc2@gmail.com>
2013-03-27 17:51:10 -0700

    refactor tests

--- coffee/test/wine.coffee
+++ coffee/test/wine.coffee
@@ -4,9 +4,13 @@ nearestNeighbor = require '../lib/main'
 describe 'K Nearest Neighbor', ->
   describe 'with basic wine testCase', ->
     testCase = require './test_case_simple'
+    wineList = testCase.wineList
+    profile1 = testCase.tasteProfile1
+    profile2 = testCase.tasteProfile2
+    profile3 = testCase.tasteProfile3
     it 'should return the nearest neighbor', ->
-      nearestNeighbor(testCase.tasteProfile1, testCase.wineList)[0].label.should.eql('C')
-      nearestNeighbor(testCase.tasteProfile2, testCase.wineList)[0].label.should.eql('E')
-      nearestNeighbor(testCase.tasteProfile3, testCase.wineList)[0].label.should.eql('J')
+      nearestNeighbor(profile1, wineList)[0].label.should.eql('C')
+      nearestNeighbor(profile2, wineList)[0].label.should.eql('E')
+      nearestNeighbor(profile3, wineList)[0].label.should.eql('J')
     it 'should return 5 nearest neighbors, sorted by distance'
     it 'should return all of Y sorted by distance if Y.length < 5'
--- test/wine.js
+++ test/wine.js
@@ -8,12 +8,16 @@
 
   describe('K Nearest Neighbor', function() {
     return describe('with basic wine testCase', function() {
-      var testCase;
+      var profile1, profile2, profile3, testCase, wineList;
       testCase = require('./test_case_simple');
+      wineList = testCase.wineList;
+      profile1 = testCase.tasteProfile1;
+      profile2 = testCase.tasteProfile2;
+      profile3 = testCase.tasteProfile3;
       it('should return the nearest neighbor', function() {
-        nearestNeighbor(testCase.tasteProfile1, testCase.wineList)[0].label.should.eql('C');
-        nearestNeighbor(testCase.tasteProfile2, testCase.wineList)[0].label.should.eql('E');
-        return nearestNeighbor(testCase.tasteProfile3, testCase.wineList)[0].label.should.eql('J');
+        nearestNeighbor(profile1, wineList)[0].label.should.eql('C');
+        nearestNeighbor(profile2, wineList)[0].label.should.eql('E');
+        return nearestNeighbor(profile3, wineList)[0].label.should.eql('J');
       });
       it('should return 5 nearest neighbors, sorted by distance');
       return it('should return all of Y sorted by distance if Y.length < 5');
