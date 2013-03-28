70fdab3a0c00eeb9c139950b85e42b866ba08e62
Marc Kuo <kuomarc2@gmail.com>
2013-03-27 17:48:33 -0700

    Add passing NN tests for wine test case

--- coffee/test/wine.coffee
+++ coffee/test/wine.coffee
@@ -3,8 +3,10 @@ nearestNeighbor = require '../lib/main'
 
 describe 'K Nearest Neighbor', ->
   describe 'with basic wine testCase', ->
-    beforeEach ->
-      testCase = require './test_case_simple'
-    it 'should return the nearest neighbor'
+    testCase = require './test_case_simple'
+    it 'should return the nearest neighbor', ->
+      nearestNeighbor(testCase.tasteProfile1, testCase.wineList)[0].label.should.eql('C')
+      nearestNeighbor(testCase.tasteProfile2, testCase.wineList)[0].label.should.eql('E')
+      nearestNeighbor(testCase.tasteProfile3, testCase.wineList)[0].label.should.eql('J')
     it 'should return 5 nearest neighbors, sorted by distance'
     it 'should return all of Y sorted by distance if Y.length < 5'
--- test/wine.js
+++ test/wine.js
@@ -8,11 +8,13 @@
 
   describe('K Nearest Neighbor', function() {
     return describe('with basic wine testCase', function() {
-      beforeEach(function() {
-        var testCase;
-        return testCase = require('./test_case_simple');
+      var testCase;
+      testCase = require('./test_case_simple');
+      it('should return the nearest neighbor', function() {
+        nearestNeighbor(testCase.tasteProfile1, testCase.wineList)[0].label.should.eql('C');
+        nearestNeighbor(testCase.tasteProfile2, testCase.wineList)[0].label.should.eql('E');
+        return nearestNeighbor(testCase.tasteProfile3, testCase.wineList)[0].label.should.eql('J');
       });
-      it('should return the nearest neighbor');
       it('should return 5 nearest neighbors, sorted by distance');
       return it('should return all of Y sorted by distance if Y.length < 5');
     });
